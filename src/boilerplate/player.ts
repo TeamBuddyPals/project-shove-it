493c2f48899805cc2c99c8bfe0212640e753ddfc
Evan Lennick <evan.lennick@accolade.com>
2019-06-09 21:57:58 -0400

    messing around with physics and making shit fly around

--- src/boilerplate/player.ts
+++ src/boilerplate/player.ts
@@ -8,6 +8,9 @@ export class Player {
                 rightKey: Phaser.Input.Keyboard.Key) {
         this.sprite = sprite;
         this.sprite.setBounce(0.1);
+        this.sprite.setMass(0.25);
+        this.sprite.setDragX(75);
+        this.sprite.setMaxVelocity(800, 0);
         this.sprite.setCollideWorldBounds(true);
         this.sprite.setGravityY(0);
 
@@ -17,9 +20,9 @@ export class Player {
 
     update() {
         if (this.leftKey.isDown) {
-            this.sprite.setAccelerationX(-400);
+            this.sprite.setAccelerationX(-600);
         } else if (this.rightKey.isDown) {
-            this.sprite.setAccelerationX(400);
+            this.sprite.setAccelerationX(600);
         } else {
             this.sprite.setAccelerationX(0);
         }
--- src/boilerplate/scenes/mainScene.ts
+++ src/boilerplate/scenes/mainScene.ts
@@ -1,8 +1,9 @@
 import {Player} from "../player";
 
 export class MainScene extends Phaser.Scene {
-    private player1;
-    private player2;
+    private player1: Player;
+    private player2: Player;
+    private bombs: Array<Phaser.Physics.Arcade.Sprite> = [];
 
     constructor() {
         super({
@@ -13,24 +14,50 @@ export class MainScene extends Phaser.Scene {
     preload(): void {
         this.load.image("greenbox", "./src/boilerplate/assets/greenbox.png");
         this.load.image("redbox", "./src/boilerplate/assets/redbox.png");
+        this.load.image("bomb", "./src/boilerplate/assets/bomb.png");
     }
 
     create(): void {
-        var p1Sprite = this.physics.add.sprite(100, 450, 'greenbox');
-        var p1LeftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-        var p1RightKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+        let p1Sprite = this.physics.add.sprite(100, 450, 'greenbox');
+        let p1LeftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        let p1RightKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
         this.player1 = new Player(p1Sprite, p1LeftKey, p1RightKey);
 
-        var p2Sprite = this.physics.add.sprite(200, 450, 'redbox');
-        var p2LeftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-        var p2rightKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        let p2Sprite = this.physics.add.sprite(200, 450, 'redbox');
+        let p2LeftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        let p2rightKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
         this.player2 = new Player(p2Sprite, p2LeftKey, p2rightKey);
 
         this.physics.add.collider(this.player1.sprite, this.player2.sprite);
+
+        this.createBomb();
+        this.createBomb();
+        this.createBomb();
+        this.createBomb();
+        this.createBomb();
+    }
+
+    private createBomb() {
+        let bomb = this.physics.add.sprite(this.generatorRandomXCoord(), 0, 'bomb');
+        bomb.setVelocity(0, 200);
+        bomb.setAcceleration(0, 0);
+        bomb.setGravity(0, 0);
+        this.bombs.push(bomb);
     }
 
     update(): void {
         this.player1.update();
         this.player2.update();
+
+        for (const bomb of this.bombs) {
+            if (bomb.y > 650) {
+                let x = this.generatorRandomXCoord();
+                bomb.setPosition(x, 0);
+            }
+        }
+    }
+
+    private generatorRandomXCoord() {
+        return Math.floor(Math.random() * Math.floor(800));
     }
 }
