c922a64707fac5ae63c3febe57bf1d5fb670aed6
Evan Lennick <evan.lennick@accolade.com>
2019-06-15 20:32:04 -0400

    stuff

--- src/boilerplate/scenes/mainScene.ts
+++ src/boilerplate/scenes/mainScene.ts
@@ -3,7 +3,10 @@ import {Player} from "../player";
 export class MainScene extends Phaser.Scene {
     private player1: Player;
     private player2: Player;
-    private bombs: Array<Phaser.Physics.Arcade.Sprite> = [];
+    private tiles: Array<Phaser.Physics.Arcade.Sprite> = [];
+    private playerPhysicsGroup: Phaser.Physics.Arcade.Group;
+    private sideWallPhysicsGroup: Phaser.Physics.Arcade.StaticGroup;
+    private tilePhysicsGroup: Phaser.Physics.Arcade.StaticGroup;
 
     constructor() {
         super({
@@ -29,9 +32,9 @@ export class MainScene extends Phaser.Scene {
         let p2rightKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
         this.player2 = new Player(p2Sprite, p2LeftKey, p2rightKey);
 
-        let playerGroup = this.physics.add.group();
-        playerGroup.add(this.player1.sprite);
-        playerGroup.add(this.player2.sprite);
+        this.playerPhysicsGroup = this.physics.add.group();
+        this.playerPhysicsGroup.add(this.player1.sprite);
+        this.playerPhysicsGroup.add(this.player2.sprite);
         this.physics.add.collider(this.player1.sprite, this.player2.sprite);
 
         let leftWall = this.physics.add.staticSprite(8, 360, 'wall');
@@ -41,37 +44,43 @@ export class MainScene extends Phaser.Scene {
         rightWall.setBounceX(0);
         rightWall.setImmovable(true);
 
-        let wallGroup = this.physics.add.staticGroup();
-        wallGroup.add(leftWall);
-        wallGroup.add(rightWall);
+        this.sideWallPhysicsGroup = this.physics.add.staticGroup();
+        this.sideWallPhysicsGroup.add(leftWall);
+        this.sideWallPhysicsGroup.add(rightWall);
 
-        this.physics.add.collider(playerGroup, wallGroup);
+        this.tilePhysicsGroup = this.physics.add.staticGroup();
 
-        // this.createTile();
+        this.physics.add.collider(this.playerPhysicsGroup, this.sideWallPhysicsGroup);
+
+        this.createTileGroup();
     }
 
-    private createTile() {
-        let bomb = this.physics.add.sprite(this.generatorRandomXCoord(), 0, 'tile');
-        bomb.setVelocity(0, 250);
-        bomb.setAcceleration(0, 0);
-        bomb.setGravity(0, 0);
-        this.bombs.push(bomb);
+    private createTile(x: number, y: number) {
+        let tile = this.physics.add.sprite(x, y, 'tile');
+        tile.setVelocity(0, 250);
+        tile.setAcceleration(0, 0);
+        tile.setGravity(0, 0);
+        this.tiles.push(tile);
+        return tile;
+    }
+
+    private createTileGroup() {
+        let x = this.generatorRandomXCoord();
+        let tile1 = this.createTile(x - 64, -32);
+        let tile2 = this.createTile(x, -32);
+        let tile3 = this.createTile(x + 64, -32);
+        this.tilePhysicsGroup.add(tile1);
+        this.tilePhysicsGroup.add(tile2);
+        this.tilePhysicsGroup.add(tile3);
+        this.physics.add.collider(this.playerPhysicsGroup, this.tilePhysicsGroup);
     }
 
     update(): void {
         this.player1.update();
         this.player2.update();
-
-        for (const bomb of this.bombs) {
-            if (bomb.y > 800) {
-                let x = this.generatorRandomXCoord();
-                bomb.setPosition(x, 0);
-            }
-        }
     }
 
     private generatorRandomXCoord() {
-        return 32;
-        // return Math.floor(Math.random() * Math.floor(16)) * 64;
+        return Math.floor(Math.random() * Math.floor(16)) * 64;
     }
 }
