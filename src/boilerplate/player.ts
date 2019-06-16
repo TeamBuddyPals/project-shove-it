44bd7dbb7fc71e5a6dd797eb1f4dfe297f7c6595
Evan Lennick <evan.lennick@accolade.com>
2019-06-16 11:24:38 -0400

    messing with tile generation and timer stuff

--- src/boilerplate/player.ts
+++ src/boilerplate/player.ts
@@ -2,20 +2,24 @@ export class Player {
     sprite: Phaser.Physics.Arcade.Sprite;
     leftKey: Phaser.Input.Keyboard.Key;
     rightKey: Phaser.Input.Keyboard.Key;
+    name: string;
 
     constructor(sprite: Phaser.Physics.Arcade.Sprite,
                 leftKey: Phaser.Input.Keyboard.Key,
-                rightKey: Phaser.Input.Keyboard.Key) {
+                rightKey: Phaser.Input.Keyboard.Key,
+                name: string) {
         this.sprite = sprite;
-        this.sprite.setBounce(0.1);
+        this.sprite.setBounce(0, 0);
         this.sprite.setMass(0.25);
         this.sprite.setDragX(75);
         this.sprite.setMaxVelocity(800, 0);
         this.sprite.setCollideWorldBounds(true);
         this.sprite.setGravityY(0);
+        this.sprite.setName(name)
 
         this.leftKey = leftKey;
         this.rightKey = rightKey;
+        this.name = name;
     }
 
     update() {
--- src/boilerplate/scenes/mainScene.ts
+++ src/boilerplate/scenes/mainScene.ts
@@ -22,47 +22,53 @@ export class MainScene extends Phaser.Scene {
     }
 
     create(): void {
+        //setup players
         let p1Sprite = this.physics.add.sprite(512, 592, 'greenbox');
         let p1LeftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         let p1RightKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-        this.player1 = new Player(p1Sprite, p1LeftKey, p1RightKey);
+        this.player1 = new Player(p1Sprite, p1LeftKey, p1RightKey, "player1");
 
         let p2Sprite = this.physics.add.sprite(768, 592, 'redbox');
         let p2LeftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         let p2rightKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
-        this.player2 = new Player(p2Sprite, p2LeftKey, p2rightKey);
+        this.player2 = new Player(p2Sprite, p2LeftKey, p2rightKey, "player2");
 
         this.playerPhysicsGroup = this.physics.add.group();
         this.playerPhysicsGroup.add(this.player1.sprite);
         this.playerPhysicsGroup.add(this.player2.sprite);
         this.physics.add.collider(this.player1.sprite, this.player2.sprite);
 
+        //setup side walls
         let leftWall = this.physics.add.staticSprite(8, 360, 'wall');
-        leftWall.setBounceX(0);
+        leftWall.setBounce(0, 0);
         leftWall.setImmovable(true);
+        leftWall.setName("leftwall")
         let rightWall = this.physics.add.staticSprite(1272, 360, 'wall');
-        rightWall.setBounceX(0);
+        rightWall.setBounce(0, 0);
         rightWall.setImmovable(true);
+        rightWall.setName("rightwall")
 
         this.sideWallPhysicsGroup = this.physics.add.staticGroup();
         this.sideWallPhysicsGroup.add(leftWall);
         this.sideWallPhysicsGroup.add(rightWall);
-
         this.tilePhysicsGroup = this.physics.add.staticGroup();
 
+        //setup additional collision detection stuff
         this.physics.add.collider(this.playerPhysicsGroup, this.sideWallPhysicsGroup);
-        this.physics.add.overlap(this.player1.sprite, this.tiles, this.player1Collision, null, this);
-        this.physics.add.overlap(this.player2.sprite, this.tiles, this.player2Collision, null, this);
+        this.physics.add.overlap(this.player1.sprite, this.tiles, this.playerCollision, null, this);
+        this.physics.add.overlap(this.player2.sprite, this.tiles, this.playerCollision, null, this);
 
-        this.createTileGroup();
+        //spawn tile groups every so often
+        this.time.addEvent({
+            delay: 2500,// ms
+            callback: this.createTileGroup,
+            callbackScope: this,
+            loop: true
+        });
     }
 
-    private player1Collision() {
-        console.log("player1 hit a tile");
-    }
-
-    private player2Collision() {
-        console.log("player2 hit a tile");
+    private playerCollision(sprite1, sprite2) {
+        console.log("collision detected between " + sprite1.name + " and " + sprite2.name);
     }
 
     private createTile(x: number, y: number) {
@@ -70,18 +76,23 @@ export class MainScene extends Phaser.Scene {
         tile.setVelocity(0, 250);
         tile.setAcceleration(0, 0);
         tile.setGravity(0, 0);
+        tile.setName("tile");
         this.tiles.push(tile);
         return tile;
     }
 
     private createTileGroup() {
         let x = this.generatorRandomXCoord();
-        let tile1 = this.createTile(x - 64, -32);
-        let tile2 = this.createTile(x, -32);
-        let tile3 = this.createTile(x + 64, -32);
+        let tile1 = this.createTile(x - 64, 0);
+        let tile2 = this.createTile(x, 64);
+        let tile3 = this.createTile(x, -64);
+        let tile4 = this.createTile(x, 0);
+        let tile5 = this.createTile(x + 64, 0);
         this.tilePhysicsGroup.add(tile1);
         this.tilePhysicsGroup.add(tile2);
         this.tilePhysicsGroup.add(tile3);
+        this.tilePhysicsGroup.add(tile4);
+        this.tilePhysicsGroup.add(tile5);
     }
 
     update(): void {
