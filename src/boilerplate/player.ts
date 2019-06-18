d616bed4c55e3376dfaf95019e7a11c1eeee3649
philkazan <philkazan@gmail.com>
2019-06-17 23:31:46 -0400

    add control scheme options
    Also leveraged access modifiers in Player class

--- src/boilerplate/player.ts
+++ src/boilerplate/player.ts
@@ -1,34 +1,64 @@
 export class Player {
-    sprite: Phaser.Physics.Arcade.Sprite;
-    leftKey: Phaser.Input.Keyboard.Key;
-    rightKey: Phaser.Input.Keyboard.Key;
-    name: string;
+    private _sprite: Phaser.Physics.Arcade.Sprite;
+    private _leftKey: Phaser.Input.Keyboard.Key;
+    private _rightKey: Phaser.Input.Keyboard.Key;
+    private _lastKeyCode: number;
 
     constructor(sprite: Phaser.Physics.Arcade.Sprite,
                 leftKey: Phaser.Input.Keyboard.Key,
                 rightKey: Phaser.Input.Keyboard.Key,
                 name: string) {
-        this.sprite = sprite;
-        this.sprite.setBounce(0, 0);
-        this.sprite.setMass(0.25);
-        this.sprite.setDragX(75);
-        this.sprite.setMaxVelocity(800, 0);
-        this.sprite.setCollideWorldBounds(true);
-        this.sprite.setGravityY(0);
-        this.sprite.setName(name)
+        this._sprite = sprite;
+        this._leftKey = leftKey;
+        this._rightKey = rightKey;
+        this._sprite.setCollideWorldBounds(true);
+        this._sprite.setName(name)
+    }
 
-        this.leftKey = leftKey;
-        this.rightKey = rightKey;
-        this.name = name;
+    getSprite(): Phaser.Physics.Arcade.Sprite {
+        return this._sprite;
+    }
+
+    showDebugDetails():void {
+        console.log(this._sprite.body);
+        console.log(this._sprite.body.acceleration);
+        console.log(this._sprite.body.velocity);
+        console.log(`lastKey: ${this._lastKeyCode}`);
+        console.log(`leftKeyDown: ${this._leftKey.isDown}`);
+        console.log(`rightKeyDown: ${this._rightKey.isDown}`);
     }
 
     update() {
-        if (this.leftKey.isDown) {
-            this.sprite.setAccelerationX(-600);
-        } else if (this.rightKey.isDown) {
-            this.sprite.setAccelerationX(600);
+        // CONTROL SCHEME - TWITCHY, RESPONSIVE
+        if (this._leftKey.isDown) {
+            this._sprite.setVelocityX(-600);
+        } else if (this._rightKey.isDown) {
+            this._sprite.setVelocityX(600);
         } else {
-            this.sprite.setAccelerationX(0);
+            this._sprite.setVelocityX(0);
         }
+
+        // CONTROL SCHEME - SOME ACCELERATION 
+        // if (this._leftKey.isDown && this._rightKey.isDown) {
+        //     this._sprite.setAccelerationX(0);
+        //     this._sprite.setVelocityX(0);
+        // } else if (this._leftKey.isDown) {
+        //     if (this._lastKeyCode && this._lastKeyCode === this._rightKey.keyCode) {
+        //         this._sprite.setVelocityX(0);
+        //         this._lastKeyCode = this._leftKey.keyCode; 
+        //     }
+        //     this._sprite.setAccelerationX(-800);
+        //     this._lastKeyCode = this._leftKey.keyCode;
+        // } else if (this._rightKey.isDown) {
+        //     if (this._lastKeyCode && this._lastKeyCode === this._leftKey.keyCode) {
+        //         this._sprite.setVelocityX(0);
+        //         this._lastKeyCode = this._rightKey.keyCode; 
+        //     }
+        //     this._sprite.setAccelerationX(800);
+        //     this._lastKeyCode = this._rightKey.keyCode;
+        // } else {
+        //         this._sprite.setAccelerationX(0);
+        //         this._sprite.setVelocityX(0);
+        // }
     }
 }
