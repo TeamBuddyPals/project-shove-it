43b4ed48cff6804c9838bd2948e90c5e3ddddd08
Evan Lennick <evan.lennick@accolade.com>
2019-06-23 13:46:42 -0400

    stubbing out menu screen a bit

--- src/boilerplate/scenes/menuScene.ts
+++ src/boilerplate/scenes/menuScene.ts
@@ -14,12 +14,18 @@ export class MenuScene extends Phaser.Scene {
         if (this._skipMenu) {
             this.scene.start("MainGameplayScene");
         }
+
+        this.load.image("title", "./src/boilerplate/assets/title.png");
     }
 
     create(): void {
-
+        let titleImage = this.add.image(650, 200, 'title');
+        this.add.text(650, 375, 'Play', { fontFamily: 'Verdana, "Times New Roman", Tahoma, serif', fontSize: '40px' });
+        this.add.text(650, 450, 'Options', { fontFamily: 'Verdana, "Times New Roman", Tahoma, serif', fontSize: '40px' });
+        this.add.text(650, 525, 'Exit', { fontFamily: 'Verdana, "Times New Roman", Tahoma, serif', fontSize: '40px' });
     }
 
     update(): void {
+
     }
 }
