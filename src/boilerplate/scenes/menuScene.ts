967fc5c0cc2fa235826ca23705afb1ab8b0f6308
Evan Lennick <evan.lennick@accolade.com>
2019-06-29 20:33:27 -0400

    implemented basic menu

--- src/boilerplate/scenes/menuScene.ts
+++ src/boilerplate/scenes/menuScene.ts
@@ -3,6 +3,12 @@ import {MainGameplayScene} from "./mainGameplayScene";
 export class MenuScene extends Phaser.Scene {
 
     private _skipMenu = false;
+    private _leftSelectorImage: Phaser.Physics.Arcade.Sprite;
+    private _leftSelectorAnimation: Phaser.GameObjects.GameObject;
+    private _rightSelectorImage: Phaser.Physics.Arcade.Sprite;
+    private _rightSelectorAnimation: Phaser.GameObjects.GameObject;
+    private _currentMenuSelection: MenuSelection = MenuSelection.Play;
+
 
     constructor() {
         super({
@@ -20,19 +26,74 @@ export class MenuScene extends Phaser.Scene {
         this.load.image("options", "./src/boilerplate/assets/image/menu/options.png");
         this.load.image("exit", "./src/boilerplate/assets/image/menu/exit.png");
         this.load.image("selector", "./src/boilerplate/assets/image/tile-mine.png");
+
+        this.load.spritesheet('selectorSpriteSheet',
+            './src/boilerplate/assets/image/spritesheet/tile-mine.png',
+            {frameWidth: 64}
+        );
     }
 
     create(): void {
-        let titleImage = this.add.image(650, 200, 'title');
-        let playImage = this.add.image(650, 415, 'play');
-        let optionsImage = this.add.image(650, 515, 'options');
-        let exitImage = this.add.image(650, 615, 'exit');
+        this.add.image(650, 200, 'title');
+        this.add.image(650, 415, 'play');
+        this.add.image(650, 515, 'options');
+        this.add.image(650, 615, 'exit');
 
-        let leftSelectorImage = this.add.image(500, 415, 'selector');
-        let rightSelectorImage = this.add.image(800, 415, 'selector');
+        this._leftSelectorImage = this.physics.add.sprite(425, 415, 'selector');
+        this._rightSelectorImage = this.physics.add.sprite(875, 415, 'selector');
+
+        this.anims.create({
+            key: 'selectorAnimation',
+            frames: this.anims.generateFrameNumbers('selectorSpriteSheet', {start: 0, end: 4}),
+            frameRate: 12,
+            repeat: -1
+        });
+
+        this.input.keyboard.on('keydown_UP', this.upSelection, this);
+        this.input.keyboard.on('keydown_DOWN', this.downSelection, this);
+        this.input.keyboard.on('keydown_SPACE', this.selectOption, this);
+        this.input.keyboard.on('keydown_ENTER', this.selectOption, this);
     }
 
     update(): void {
-
+        this._leftSelectorAnimation = this._leftSelectorImage.anims.play('selectorAnimation', true);
+        this._rightSelectorAnimation = this._rightSelectorImage.anims.play('selectorAnimation', true);
     }
+
+    private upSelection(event) {
+        let currentY = this._leftSelectorImage.y;
+        if (currentY < 450) {
+            return;
+        }
+        this._leftSelectorImage.setY(currentY - 100);
+        this._rightSelectorImage.setY(currentY - 100);
+        this._currentMenuSelection--;
+    }
+
+    private downSelection(event) {
+        let currentY = this._leftSelectorImage.y;
+        if (currentY > 600) {
+            return;
+        }
+        this._leftSelectorImage.setY(currentY + 100);
+        this._rightSelectorImage.setY(currentY + 100);
+        this._currentMenuSelection++;
+    }
+
+    private selectOption(event) {
+        console.log("menu option selected");
+        if (this._currentMenuSelection === MenuSelection.Play) {
+            this.scene.start("MainGameplayScene");
+        } else if (this._currentMenuSelection === MenuSelection.Options) {
+            //todo implement this
+        } else {
+            this.game.destroy(true);
+        }
+    }
+}
+
+enum MenuSelection {
+    Play,
+    Options,
+    Exit
 }
