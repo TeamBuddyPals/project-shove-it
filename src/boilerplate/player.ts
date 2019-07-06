8670d0b699d37a405fadeea30d1b811243640b07
Phil <philkazan@gmail.com>
2019-07-06 12:41:27 -0400

    add abilities

--- src/boilerplate/player.ts
+++ src/boilerplate/player.ts
@@ -3,14 +3,32 @@ export class Player {
     private _leftKey: Phaser.Input.Keyboard.Key;
     private _rightKey: Phaser.Input.Keyboard.Key;
     private _lastKeyCode: number;
+    private _name: string;
+    // Dash props
+    private _dashKey: Phaser.Input.Keyboard.Key;
+    private _isDashing: boolean = false;
+    private _dashSpeed: number = 3000;
+    private _dashCooldown: number = 1000;
+    private _dashDuration: number = 120;
+    private _canDash: boolean = true;
+    // Movement Lock props
+    private _mLockKey: Phaser.Input.Keyboard.Key;
+    private _mlockDuration: number = 500;
+    private _mlockCooldown: number = 1000;
+    private _canMLock: boolean = true;
+    private _isMLocking: boolean = false;
 
     constructor(sprite: Phaser.Physics.Arcade.Sprite,
                 leftKey: Phaser.Input.Keyboard.Key,
                 rightKey: Phaser.Input.Keyboard.Key,
+                dashKey: Phaser.Input.Keyboard.Key,
+                mLockKey: Phaser.Input.Keyboard.Key,
                 name: string) {
         this._sprite = sprite;
         this._leftKey = leftKey;
         this._rightKey = rightKey;
+        this._dashKey = dashKey;
+        this._mLockKey = mLockKey;
         this._sprite.setCollideWorldBounds(true);
         this._sprite.setName(name)
     }
@@ -19,24 +37,107 @@ export class Player {
         return this._sprite;
     }
 
+    performMLock() {
+        console.log('PERFORM - MLOCK');
+        this._sprite.body.velocity.x = 0;
+        this._sprite.body.immovable = true;
+        this._isMLocking = true;
+        this._canMLock = false;
+        setTimeout(() => {
+            console.log('MLOCK - DURATION EXPIRED');
+            this._isMLocking = false;
+            this._sprite.body.immovable = false;
+        }, this._mlockDuration);
+        this.coolDownMLock();
+    }
+
+    disableDash() {
+        this._canDash = false;
+    }
+
+    enableDash() {
+        this._canDash = true;
+    }
+
+    performDash() {
+        console.log('DASH - PERFORM ');
+        const currentVelocityX = this._sprite.body.velocity.x;
+        if (this._leftKey.isDown) {
+            console.log('DASH - LEFT DASH');
+            this._sprite.setVelocityX( currentVelocityX - this._dashSpeed);
+            this._isDashing = true;
+            this._canDash = false;
+            setTimeout( () => {
+                this._isDashing = false;
+            }, this._dashDuration);
+            this.coolDownDash();
+        } else if (this._rightKey.isDown) {
+            console.log('DASH - RIGHT DASH');
+            this._sprite.setVelocityX( currentVelocityX + this._dashSpeed );
+            this._isDashing = true;
+            this._canDash = false;
+            setTimeout( () => {
+                console.log('DASH - DURATION EXPIRED');
+                this._isDashing = false;
+            }, this._dashDuration);
+            this.coolDownDash();
+        }
+    }
+
+    private coolDownDash(): void {
+        setTimeout(() => {
+            console.log(`DASH - HAS COOLED DOWN`);
+            this._canDash = true;
+        }, this._dashCooldown);
+    }
+
+    private coolDownMLock(): void {
+        setTimeout(() => {
+            console.log(`MLOCK - HAS COOLED DOWN`);
+            this._canMLock = true;
+        }, this._mlockCooldown);
+    }
+
     showDebugDetails(): void {
-        console.log(this._sprite.body);
-        console.log(this._sprite.body.acceleration);
+        console.log(`DEBUG - ${this._sprite.name}`);
+        // console.log(this._sprite.body);
+        // console.log(this._sprite.body.acceleration);
         console.log(this._sprite.body.velocity);
-        console.log(`lastKey: ${this._lastKeyCode}`);
-        console.log(`leftKeyDown: ${this._leftKey.isDown}`);
-        console.log(`rightKeyDown: ${this._rightKey.isDown}`);
+        // console.log(`lastKey: ${this._lastKeyCode}`);
+        // console.log(`leftKeyDown: ${this._leftKey.isDown}`);
+        // console.log(`rightKeyDown: ${this._rightKey.isDown}`);
     }
 
     update() {
         // CONTROL SCHEME - TWITCHY, RESPONSIVE
-        if (this._leftKey.isDown) {
-            this._sprite.setVelocityX(-600);
-        } else if (this._rightKey.isDown) {
-            this._sprite.setVelocityX(600);
-        } else {
-            this._sprite.setVelocityX(0);
+        this.showDebugDetails();
+        if (!this._isMLocking) {
+            if (!this._isDashing) {
+                if (this._mLockKey.isDown && this._canMLock) {
+                    this.performMLock();
+                } else if (this._leftKey.isDown) {
+                    if (this._dashKey.isDown && this._canDash) {
+                        this.performDash();
+                    } else {
+                        if (this._sprite.body.velocity.x !== -600) {
+                            this._sprite.setVelocityX(-600);
+                        }
+                    }
+                } else if (this._rightKey.isDown) {
+                    if (this._dashKey.isDown && this._canDash) {
+                        this.performDash();
+                    } else {
+                        if (this._sprite.body.velocity.x !== 600) {
+                            this._sprite.setVelocityX(600);
+                        }
+                    }
+                } else {
+                    this._sprite.setVelocityX(0);
+                }
+            }
         }
+    }
+}
 
         // CONTROL SCHEME - SOME ACCELERATION 
         // if (this._leftKey.isDown && this._rightKey.isDown) {
@@ -60,5 +161,3 @@ export class Player {
         //         this._sprite.setAccelerationX(0);
         //         this._sprite.setVelocityX(0);
         // }
-    }
-}
--- src/boilerplate/scenes/mainGameplayScene.ts
+++ src/boilerplate/scenes/mainGameplayScene.ts
@@ -36,46 +36,73 @@ export class MainGameplayScene extends Phaser.Scene {
         let p1Sprite = this.physics.add.sprite(512, 592, 'greenbox');
         let p1LeftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         let p1RightKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-        this._player1 = new Player(p1Sprite, p1LeftKey, p1RightKey, "player1");
+        let p1DashKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        let p1MLockKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.T);
+        this._player1 = new Player(
+            p1Sprite,
+            p1LeftKey,
+            p1RightKey,
+            p1DashKey,
+            p1MLockKey,
+            "player1");
         this._player1.sprite.setDisplaySize(64, 64);
 
         let p2Sprite = this.physics.add.sprite(768, 592, 'redbox');
         let p2LeftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         let p2rightKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
-        this._player2 = new Player(p2Sprite, p2LeftKey, p2rightKey, "player2");
+        let p2DashKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.M);
+        let p2MLockKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.N);
+
+        this._player2 = new Player(
+            p2Sprite,
+            p2LeftKey,
+            p2rightKey,
+            p2DashKey,
+            p2MLockKey,
+            "player2");
         this._player2.sprite.setDisplaySize(64, 64);
 
         this._playerPhysicsGroup.add(this._player1.sprite);
         this._playerPhysicsGroup.add(this._player2.sprite);
-        this.physics.add.collider(this._player1.sprite, this._player2.sprite);
+        let collder = this.physics.add.collider(
+            this._player1.sprite,
+            this._player2.sprite,
+            () => {
+                this.physics.
+                this._player1.disableDash();
+                this._player2.disableDash();
+                // disable dash ability on collision between players
+            },
+            null,
+            this);
+            
+        // this._levelManager.create();
+        // this.physics.add.collider(this._playerPhysicsGroup, this._levelManager.sideWallPhysicsGroup);
 
-        this._levelManager.create();
-        this.physics.add.collider(this._playerPhysicsGroup, this._levelManager.sideWallPhysicsGroup);
+        // this.anims.create({
+        //     key: 'explosionAnimation',
+        //     frames: this.anims.generateFrameNumbers('explosionSpriteSheet', {start: 1, end: 14}),
+        //     frameRate: 25,
+        //     repeat: 2
+        // });
 
-        this.anims.create({
-            key: 'explosionAnimation',
-            frames: this.anims.generateFrameNumbers('explosionSpriteSheet', {start: 1, end: 14}),
-            frameRate: 25,
-            repeat: 2
-        });
-
-        this.sound.add('explosionSound');
+        // this.sound.add('explosionSound');
     }
 
     update(): void {
         this._player1.update();
         this._player2.update();
         this._levelManager.update();
-        this.physics.overlap(this._playerPhysicsGroup, this._levelManager.getAllTileSprites(), this.playerDestroyed, this.checkIfPlayerHitBottomOfTile, this);
+        // this.physics.overlap(this._playerPhysicsGroup, this._levelManager.getAllTileSprites(), this.playerDestroyed, this.checkIfPlayerHitBottomOfTile, this);
     }
 
-    private checkIfPlayerHitBottomOfTile(sprite1, sprite2): boolean {
-        let xDif = Math.abs(sprite1.x - sprite2.x);
-        if (xDif < 64) {
-            return true;
-        }
-        return false;
-    }
+    // private checkIfPlayerHitBottomOfTile(sprite1, sprite2): boolean {
+    //     let xDif = Math.abs(sprite1.x - sprite2.x);
+    //     if (xDif < 64) {
+    //         return true;
+    //     }
+    //     return false;
+    // }
 
     private playerDestroyed(sprite1, sprite2): void {
         console.log("collision detected between " + sprite1.name + " and " + sprite2.name);
